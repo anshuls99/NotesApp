22bea9856b5c42b5fae80ddfa3b26f7f446d9d48
anshul <anshsharma4650@gmail.com>
2020-11-09 21:54:06 +0530

    Added The Feature to add and delete note

--- app/src/main/java/com/example/notes/Note.kt
+++ app/src/main/java/com/example/notes/Note.kt
@@ -6,5 +6,5 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "notes_table")
 class Note(@ColumnInfo(name = "text") val text: String) {
-    @PrimaryKey(autoGenerate = true)var id = 0
+    @PrimaryKey(autoGenerate = true) var id = 0
 }
